9195a6b2ead8ad410d39b6d53e5d89bd37ec3aee
soup-bowl <soup-bowl@users.noreply.github.com>
2018-05-22 16:23:52 -0400

    php-di used to manage dependencies

--- app/hooks.php
+++ app/hooks.php
@@ -15,13 +15,12 @@ class hooks {
 	protected $settings;
 	protected $fields;
 
-	public function __construct() {
-		$this->taxonomy = new taxonomy();
-		$this->publish  = new publish();
-		$this->delete   = new delete();
-		$this->settings = new settings();
-		$this->fields   = new fields();
-		
+	public function __construct(taxonomy $taxonomy, publish $publish, delete $delete, settings $settings, fields $fields) {
+		$this->taxonomy = $taxonomy;
+		$this->publish  = $publish;
+		$this->delete   = $delete;
+		$this->settings = $settings;
+		$this->fields   = $fields;	
 	}
 
 	public function main() {
@@ -32,24 +31,26 @@ class hooks {
 		add_action( 'add_meta_boxes_kebabble_orders', [&$this->fields, 'orderOptionsSetup'] );
 		
 		// Resource queue.
-		add_action( 'admin_enqueue_scripts', function() { $this->enqueuedScripts(); });
+		add_action( 'admin_enqueue_scripts', function() { 
+			$this->enqueuedScripts(); 
+		});
 		
 		// Order functionalities.
-		add_action( 'publish_kebabble_orders', [&$this->publish, 'handlePublish'], 10, 2 );
-		add_filter( 'wp_insert_post_data',     [&$this->publish, 'changeTitle'],   99, 2 );
-		add_action( 'trash_kebabble_orders',   [&$this->delete, 'handleDeletion'], 10, 2 );
+		add_action( 'publish_kebabble_orders', [&$this->publish, 'handlePublish'],    10, 2 );
+		add_filter( 'wp_insert_post_data',     [&$this->publish, 'changeTitle'],      99, 2 );
+		add_action( 'trash_kebabble_orders',   [&$this->delete,  'handleDeletion'],   10, 2 );
+		//add_action( 'untrash_kebabble_orders', [&$this->delete,  'handleUndeletion'], 10, 2 );
 	}
 
 	private function settings() {
-		$newSettings = new settings();
 		add_action( 'admin_menu', [&$this->settings, 'page'] );
 		add_action( 'admin_init', [&$this->settings, 'settings'] );
 	}
 	
 	private function enqueuedScripts() {
 		if(get_current_screen()->id == 'kebabble_orders') {
-			wp_enqueue_style( 'kebabble-orders-css', plugins_url('/../resource/orders.css', __FILE__), array(), '1.1' );
-			wp_enqueue_script('kebabble-orders-js', plugins_url('/../resource/orders.js', __FILE__), array('jquery'));
+			wp_enqueue_style( 'kebabble-orders-css', plugins_url('/../resource/orders.css', __FILE__), [],         '1.1');
+			wp_enqueue_script('kebabble-orders-js',  plugins_url('/../resource/orders.js', __FILE__),  ['jquery'], '1.0');
 		}
 	}
 	
--- plugin.php
+++ plugin.php
@@ -4,9 +4,9 @@ Plugin Name: Kebabble
 Description: Office food management system. Depends on ACF (comes packaged-in).
 Author: soup-bowl
 Author URI: https://github.com/soup-bowl/kebabble
-Version: 0.1.6 Alpha
+Version: 0.1.7 Alpha
 */
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$hookup = (new kebabble\hooks())->main();
+(new DI\Container())->get('kebabble\hooks')->main();
