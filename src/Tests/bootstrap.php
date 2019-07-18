10f7b76bcfb581c9d9c3e198db957bda4823bfab
soup-bowl <code@soupbowl.io>
2019-07-18 09:31:39 +0100

    Fix broken unit testing.

--- src/Tests/bootstrap.php
+++ src/Tests/bootstrap.php
@@ -11,4 +11,15 @@
  * Composer.
  */
 require_once __DIR__ . '/../../vendor/autoload.php';
+
+// Replicate the WordPress option functionality used by Kebabble to retrieve settings.
+function get_option( $option ) {
+	return [
+		'kbfos_botkey'      => 'xorb-somefakestring',
+		'kbfos_botchannel'  => '#example',
+		'kbfos_pullthrough' => 1,
+		'kbfos_payopts'     => 'Cash, Other'
+	];
+}
+
 WP_Mock::bootstrap();
