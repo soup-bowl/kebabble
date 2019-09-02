57bbabfd37d0fb6c3db0186d5631f89dd68cbe2b
soup-bowl <code@soupbowl.io>
2019-09-02 19:22:27 +0100

    Table shows expiry status.

--- src/Config/Table.php
+++ src/Config/Table.php
@@ -11,6 +11,8 @@ namespace Kebabble\Config;
 
 use Kebabble\Library\Slack;
 
+use Carbon\Carbon;
+
 /**
  * Handles additional admin table display information.
  */
@@ -48,7 +50,8 @@ class Table {
 	 * @return string[] The inputted array is returned with modifications.
 	 */
 	public function orders_column_definition( array $columns ):array {
-		$columns['kebabble_channel'] = 'Channel';
+		$columns['kebabble_channel']      = 'Channel';
+		$columns['kebabble_order_status'] = 'Status';
 
 		return $columns;
 	}
@@ -61,16 +64,36 @@ class Table {
 	 * @return void Apparently it magically understands the changes.
 	 */
 	public function orders_table_data( string $column, int $post_id ):void {
-		if ( $column === 'kebabble_channel' ) {
-			$chosen   = get_post_meta( $post_id, 'kebabble-slack-channel', true );
-			$channels = $this->slack->channels();
-			$selected = 'Unknown';
-			foreach ( $channels as $channel ) {
-				if ( $channel['key'] === $chosen ) {
-					$selected = $channel['channel'];
+		switch ( $column ) {
+			case 'kebabble_channel':
+				$chosen   = get_post_meta( $post_id, 'kebabble-slack-channel', true );
+				$channels = $this->slack->channels();
+				$selected = 'Unknown';
+
+				foreach ( $channels as $channel ) {
+					if ( $channel['key'] === $chosen ) {
+						$selected = $channel['channel'];
+					}
 				}
-			}
-			echo esc_textarea( $selected );
+
+				echo esc_textarea( $selected );
+				break;
+			case 'kebabble_order_status':
+				$expiry = get_post_meta( $post_id, 'kebabble-timeout', true );
+
+				if ( ! empty( $expiry ) ) {
+					$kses_allow = [ 'span' => [ 'style' => [] ] ];
+					if ( $expiry >= Carbon::now()->timestamp ) {
+						echo wp_kses( '<span style="color:green;">Active</span>', $kses_allow );
+					} else {
+						echo wp_kses( '<span style="color:red;">Closed</span>', $kses_allow );
+					}
+				} else {
+					echo wp_kses( 'N/A', [] );
+				}
+				break;
+			default:
+				break;
 		}
 	}
 }
