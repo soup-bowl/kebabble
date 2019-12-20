2d9b133b76560785cd14bc7249e8a16a5cd0ddb4
soup-bowl <code@soupbowl.io>
2019-12-20 13:25:06 +0000

    More order control handed over to Botman.

--- src/API/Mention.php
+++ src/API/Mention.php
@@ -83,7 +83,7 @@ class Mention {
 	}
 
 	/**
-	 * Initial function for the Slack Events segment of the API.
+	 * Handles the receipt of the API call, and passes into Botman.
 	 *
 	 * @param WP_REST_Request $request Object from WordPress API comms.
 	 * @return array Will be JSON-ified and sent to the endpoint.
@@ -102,6 +102,12 @@ class Mention {
 				'token' => $this->slack->get_auth(),
 			]
 		]);
+
+		$order_obj = $this->get_latest_order( $request['event']['channel'] );
+		$order     = $this->orderstore->get( $order_obj->ID );
+		$places    = wp_get_object_terms( $order_obj->ID, 'kebabble_company' );
+		$place     = ( isset( $places ) ) ? $places[0] : null;
+		$menu      = '(' . implode( '|', $this->get_potentials( $place->term_id ) ) . ')';
 
 		$kebabble_user = $this->slack->get_bot_details()['user_id'];
 		$kebabble_tag  = "<@{$kebabble_user}>";
@@ -129,109 +135,40 @@ class Mention {
 			}
 		});
 
+		$p = $this->publish;
+		$c = true;
+		$botman->hears( "{$kebabble_tag} .*remove .*{$menu}.*", function ( BotMan $bot, string $order ) use ( &$c, &$order_obj, &$request, &$p ) {
+			if ( $c === true ) {
+				$response = $this->remove_from_order( $order_obj->ID, $order, $request['event']['user'] );
+				$p->handle_publish( $order_obj, false );
+
+				$c = false;
+				if ( $response ) {
+					$bot->reply( 'Success' );
+				} else {
+					$bot->reply( 'Failed' );
+				}
+			}
+		});
+		
+		$botman->hears( "{$kebabble_tag} .*{$menu}.*", function ( BotMan $bot, string $order ) use ( &$c, &$order_obj, &$request, &$p ) {
+			if ( $c === true ) {
+				$response = $this->add_to_order( $order_obj->ID, $order, $request['event']['user'] );
+				$p->handle_publish( $order_obj, false );
+
+				$c = false;
+				if ( $response ) {
+					$bot->reply( 'Success' );
+				} else {
+					$bot->reply( 'Failed' );
+				}
+			}
+		});
+
 		// start listening
 		$botman->listen();
 
-		//$this->slack->send_message(
-		//	"```\n" . 'Ended sucessfully.' . "\n```",
-		//	null,
-		//	$request['event']['channel'],
-		//	$request['event']['ts']
-		//);
-
-		//if ( ! empty( $request['event'] ) && $request['event']['type'] === 'app_mention' ) {
-			/* Begin a new order.
-			if ( preg_match( '/\bnew order\b/', strtolower( $request['event']['text'] ) ) ) {
-				preg_match( '/(?<=at ).*$/i', $request['event']['text'], $place );
-				$this->new_order(
-					$request['event']['channel'],
-					( ! empty( $place ) ) ? $place[0] : null,
-					$request['event']['user']
-				);
-
-				return [];
-			}
-
-			// Grabs the latest order (or reports back if non-existent).
-			$order_obj = $this->get_latest_order( $request['event']['channel'] );
-			if ( empty( $order_obj ) ) {
-				$this->slack->send_message(
-					$this->message( 4 ),
-					null,
-					$request['event']['channel'],
-					$request['event']['ts']
-				);
-
-				return [];
-			}
-
-			// Close an existing order (if created by the user).
-			if ( preg_match( '/\bclose order\b/', strtolower( $request['event']['text'] ) ) ) {
-				$collector = wp_get_object_terms( $order_obj->ID, 'kebabble_collector' );
-				$collector = ( ! empty( $collector ) ) ? get_term_meta( $collector[0]->term_id, 'keabble_collector_slackcode', true ) : null;
-
-				if ( $collector === $request['event']['user'] ) {
-					update_post_meta( $order_obj->ID, 'kebabble-timeout', Carbon::now()->timestamp );
-
-					$this->slack->react( Emojis::positive(), $request['event']['ts'], $request['event']['channel'] );
-				} else {
-					$this->slack->send_message(
-						'You do not have permission to cancel this order.',
-						null,
-						$request['event']['channel'],
-						$request['event']['ts']
-					);
-				}
-
-				return [];
-			}
-
-			$places = wp_get_object_terms( $order_obj->ID, 'kebabble_company' );
-			$place  = ( isset( $places ) ) ? $places[0] : null;
-			$order  = $this->orderstore->get( $order_obj->ID );
-
-			$response = $this->informational_commands(
-				$request['event']['text'],
-				$request['event']['user'],
-				$place
-			);
-
-			// Detect command to change the collector.
-			$new_driver = $this->change_collector( $request['event']['text'], $order );
-			if ( isset( $new_driver ) ) {
-				update_post_meta( $order_obj->ID, 'kebabble-driver', $new_driver );
-				$this->publish->handle_publish( $order_obj, false );
-				$this->slack->react( Emojis::positive( 2 ), $request['event']['ts'], $request['event']['channel'] );
-
-				return [];
-			}
-
-			if ( isset( $response ) ) {
-				$this->slack->send_message(
-					$response,
-					null,
-					$request['event']['channel'],
-					$request['event']['ts']
-				);
-			} else {
-				$result = $this->process_input_request(
-					$order_obj->ID,
-					$request['event']['user'],
-					$request['event']['text'],
-					$place
-				);
-
-				if ( $result['success'] ) {
-					update_post_meta( $order_obj->ID, 'kebabble-order', $result['order'] );
-					$this->publish->handle_publish( $order_obj, false );
-					$this->slack->react( Emojis::positive( 0 ), $request['event']['ts'], $request['event']['channel'] );
-				} else {
-					$this->slack->react( Emojis::negative( 0 ), $request['event']['ts'], $request['event']['channel'] );
-				}
-			}*/
-
-			return [];
-		//}
+		return [];
 	}
 
 	private function get_order_details( string $channel ) {
@@ -250,88 +187,33 @@ class Mention {
 		}
 	}
 
-	/**
-	 * Processes the request contents and modifies the order accordingly.
-	 *
-	 * @param integer $post_id Currently working Order.
-	 * @param string  $user    Slack user code.
-	 * @param string  $request The whole message string that's been sent to our bot.
-	 * @param WP_Term $place   The place to operate with.
-	 * @return array Choices reflect on the current post and the Slack channel.
-	 */
-	public function process_input_request( int $post_id, string $user, string $request, ?WP_Term $place ):array {
+	public function add_to_order( int $post_id, string $item, string $person ) {
 		$order = get_post_meta( $post_id, 'kebabble-order', true );
 
-		// Split up the presence of commas, and remove the @kebabble call. A better way would be appreciated.
-		$message_split = explode( ',', str_replace( '<>', '', preg_replace( '/@\w+/', '', strtolower( $request ), 1 ) ) );
+		$order[] = [
+			'person' => "SLACK_{$person}",
+			'food'   => $item,
+		];
 
-		$messages = [];
-		foreach ( $message_split as $message_segment ) {
-			$messages[] = $this->order_parser->decipherOrder(
-				$message_segment,
-				( isset( $place ) ) ? $this->get_potentials( $place->term_id ) : []
-			);
-		}
+		update_post_meta( $post_id, 'kebabble-order', $order );
 
-		$order_items   = $order;
-		$order_count   = count( $order );
-		$success_count = 0;
-		// Each segment of a multiple request. Normally only one.
-		foreach ( $messages as $message ) {
-			if ( empty( $message ) ) {
-				return [
-					'success' => false,
-					'order'   => $order,
-				];
-			}
+		return true;
+	}
 
-			$name = ( null === $message->getFor() ) ? "SLACK_{$user}" : $message->getFor();
-			// Iterate through existing order, check for duplicates and removals.
-			$already_removed = false;
-			for ( $i = 0; $i < $order_count; $i++ ) {
-				switch ( $message->getOperator() ) {
-					default:
-					case 'add':
-						if ( ( $i + 1 ) === $order_count ) {
-							$order_items[] = [
-								'person' => $name,
-								'food'   => $message->getItem(),
-							];
-						}
-						break;
-					case 'remove':
-						if ( isset( $order_items[ $i ] ) && strtolower( $order_items[ $i ]['person'] ) === strtolower( $name ) && ! $already_removed ) {
-							unset( $order_items[ $i ] );
-							$already_removed = true;
-						}
-						break;
-				}
-				$success_count++;
-			}
+	public function remove_from_order( int $post_id, string $item, string $person ) {
+		$order = get_post_meta( $post_id, 'kebabble-order', true );
 
-			if ( 0 === $order_count && 'add' === $message->getOperator() ) {
-				$order_items[] = [
-					'person' => $name,
-					'food'   => $message->getItem(),
-				];
-				$success_count++;
+		foreach ( $order as $key => $food ) {
+			if ( $food['person'] === "SLACK_{$person}" && $food['food'] === $item ) {
+				unset( $order[$key] );
+
+				update_post_meta( $post_id, 'kebabble-order', $order );
+
+				return true;
 			}
 		}
 
-		if ( $success_count > 0 ) {
-			unset( $order );
-			$order = array_values( $order_items );
-
-			return [
-				'success' => true,
-				'order'   => $order,
-			];
-		} else {
-			return [
-				'success' => false,
-				'order'   => $order,
-			];
-		}
+		return false;
 	}
 
 	/**
